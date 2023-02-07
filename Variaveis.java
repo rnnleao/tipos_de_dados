e01a935c9ddd2e151f0fe934ea80ebfcda58b72b
rnnleao <renancleao@outlook.com>
2023-02-07 15:16:43 +0000

    salvando o arquivo exemplo variaveis

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,14 @@
 public class Variaveis {
+    public static void main(String[] args) {
 
-}
+    
+    int idade=17;
+    char letra='M';
+    boolean vivo=false;
+    byte quantidadeBola=7;
+    double salario = 1302.00;
+    float salarioMenor =1212;
+    short alunosPalmital = 10000;
+    long populacaoPalmital = 22000;
+    System.out.println(idade);
+}}
