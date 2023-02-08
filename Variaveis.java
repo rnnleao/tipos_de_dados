9ccc97358ddf2c0e72a80c60fbd3afecb2cb44b1
rnnleao <renancleao@outlook.com>
2023-02-08 17:04:24 +0000

    variaveis salvas

--- Variaveis.java
+++ Variaveis.java
@@ -7,8 +7,20 @@ public class Variaveis {
     boolean vivo=false;
     byte quantidadeBola=7;
     double salario = 1302.00;
-    float salarioMenor =1212;
+    float salarioMenor =1212.00f;
     short alunosPalmital = 10000;
     long populacaoPalmital = 22000;
+    
+    String nome;
+    nome="Renan";
+    System.out.println("Meun nome é: "+nome + ", tenho: "+idade+" anos");
+
     System.out.println(idade);
+    System.out.println(letra);
+    System.out.println(vivo);
+    System.out.println(quantidadeBola);
+    System.out.println(salario);
+    System.out.println(salarioMenor);
+    System.out.println(alunosPalmital);
+    System.out.println(populacaoPalmital);
 }}
