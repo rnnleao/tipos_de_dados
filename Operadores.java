5ef24051724e3beec64cd592a806ee05a3ee9004
rnnleao <renancleao@outlook.com>
2023-02-14 14:12:23 +0000

    Operadores

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,13 @@
-public class Operadores.java {
-
+public class Operadores {
+    public static void main(String[] renan){
+    
+        int idade;
+        idade = 16;
+        idade = 17;
+        idade++;
+        idade++;
+        System.out.println("Eu tenho "+ idade +" anos");
+        idade+=3;
+        System.out.println(idade);
+    }
 }
