4599a0981c644926f3e59c32bf38c7d68d54aac7
rnnleao <renancleao@outlook.com>
2023-02-14 14:52:18 +0000

    exercicio

---
+++ OperadorCalc.java
@@ -0,0 +1,32 @@
+public class OperadorCalc {
+    // colete dois números do teclado
+    //faça as 4 operações matemáticas
+    public static void main(String[] renan){
+        Scanner entrada = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int soma;
+
+        System.out.println("Digite um número");
+        numero1=entrada.nextInt();
+
+        System.out.println("Digite um número");
+        numero2=entrada.nextInt();
+        soma=numero1+numero2;
+        System.out.println("A soma dos dois números são " +soma);
+    }
+
+    Scanner entrada = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int subtracao;
+
+        System.out.println("Digite um número");
+        numero1=entrada.nextInt();
+
+        System.out.println("Digite um número");
+        numero2=entrada.nextInt();
+        soma=numero1+numero2;
+        System.out.println("A soma dos dois números são " +subtracao);
+    }
+}
